b59cba5373b1d3172689722c12741c49c9d06628
Christian <cgulick@student.fitchburgstate.edu>
2021-10-20 21:37:38 -0400

    Materials, Player Movement

--- 2GameJam2021/Assets/Scripts/PlayerController.cs
+++ 2GameJam2021/Assets/Scripts/PlayerController.cs
@@ -6,24 +6,23 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject player;
     private float speed;
-    private CharacterController controller;
+    
     // Start is called before the first frame update
     void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
-        speed = 5;
+        
+        speed = .02f;
     }
 
     // Update is called once per frame
     void Update()
     {
-         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * speed);
+       float horizontalInput = Input.GetAxis("Horizontal");
+       float verticalInput = Input.GetAxis("Vertical");
 
-         if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
+       Vector3 moveDirection = new Vector3(horizontalInput, 0.0f, verticalInput);
+
+       transform.position += moveDirection * speed;
 
 
 
